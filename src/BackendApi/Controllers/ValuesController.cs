00f66390ff728f985f60a8ea555887887e546879
puzatkin-ivan <puzatkin1998@gmail.com>
2019-03-06 09:56:37 +0300

    Лабораторная работа #3

---
+++ src/BackendApi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+using StackExchange.Redis;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+        private static readonly ConcurrentDictionary<string, string> _data = new ConcurrentDictionary<string, string>();
+        // GET api/values/<id>
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            string value = null;
+            _data.TryGetValue(id, out value);
+            return value;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            string id = Guid.NewGuid().ToString();
+            _data[id] = value;
+            IDatabase db = redis.GetDatabase();
+            db.StringSet(id, value);
+            var subscriber = redis.GetSubscriber();
+            subscriber.Publish("events", id);
+            return id;
+        }
+    }
+}
---
+++ src/TextListener/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Configuration;
+
+namespace TextListener
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Console Text Listener is running.");
+            try
+            {
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+                IDatabase redisDb = redis.GetDatabase();
+                ISubscriber sub = redis.GetSubscriber();
+                sub.Subscribe("textCreated", (channel, message) => 
+                {
+                    Console.WriteLine("TextCreated: " +  message);
+                });
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
